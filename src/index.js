c36878d6092459204f32020c731d6ae58ba197c8
NazarProton <79938183+NazarProton@users.noreply.github.com>
2021-12-10 17:03:16 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import './index.css';
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+
+const element = <h1>Hello World меня зовут: Proton Nazar</h1>;
+
+console.log(element);
+ReactDOM.render(element, document.getElementById("root"));
